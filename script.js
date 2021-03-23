8ce9b5e7d84be558cd7a8fad789395103d2beaee
Paul Fleming <paul.stillbreathing@gmail.com>
2021-03-23 20:56:03 +0000

    update to sound board

---
+++ script.js
@@ -0,0 +1,25 @@
+const sounds = ['happy-days', 'telt', 'woohoo', 'do-the-thing', 'silly-customers', 'tada']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        //stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
